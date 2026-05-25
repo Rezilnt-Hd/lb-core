6b8b52ad498e72d6bb823b7cf38b5f107feac74d
sduga <sduga@localbuilder.com>
2026-05-25 11:11:29 -0500

    feat: public barrel exports, all tests passing

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// Types
+export { Lead, LeadStatus, VALID_TRANSITIONS, StatusTransition } from './types/lead.js';
+export { Site, BlogPost, KeywordData } from './types/site.js';
+export { Template } from './types/template.js';
+export {
+  EVENT_SOURCE, EventType,
+  LeadStatusChangedPayload, SiteBuiltPayload,
+  PaymentCompletedPayload, DomainProvisionedPayload,
+} from './types/events.js';
+
+// DynamoDB clients
+export { createLead, getLead, transitionLead, getLeadsByStatus } from './dynamo/leads.js';
+export { docClient, TABLE_NAMES } from './dynamo/client.js';
+
+// Events
+export { publishEvent } from './events/publisher.js';
+export { PATTERNS } from './events/patterns.js';
+
+// Utilities
+export { generateSlug } from './utils/slug.js';
+export { createLogger } from './utils/logger.js';
